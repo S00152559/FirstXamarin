01e16a0105d8990b57323ae7eeb774bd1f4e05c1
S00152559 <S00152559@mail.itsligo.ie>
2018-11-21 19:39:21 +0000

    first

---
+++ XamarinVideo/XamarinVideo/XamarinVideo/EditingPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinVideo
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class EditingPage : ContentPage
+	{
+		public EditingPage ()
+		{
+			InitializeComponent ();
+
+            listVideo.ItemsSource = new List<VideoImage>
+            {
+               new VideoImage{Name = "Screen 1",ImageUrl = "https://www.hublot.com/images/News_2018/match_for_solidarity_gallery_13.jpg"},
+               new VideoImage{Name = "Screen 2",ImageUrl="https://englandexplore.com/wp-content/uploads/tips-americans-know-before-football-4.jpg"},
+               new VideoImage{Name = "Screen 3",ImageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKhit_qTO-Gl0EdpB8qewoEcHgcInnBev-amDwGJfHqL0O7VvA"}
+            };
+		}
+	}
+}
---
+++ XamarinVideo/XamarinVideo/XamarinVideo/VideoImage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinVideo
+{
+    class VideoImage
+    {
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
